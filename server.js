e518e0d9215bcabe0ed7c8920a2c97a8cf2ed68b
tashaxanavsem82 <swgwull@hotmail.com>
2025-10-06 13:16:36 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/collaborativenotes', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Collaborative Notes API');
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
